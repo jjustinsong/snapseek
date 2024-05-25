7329fc0b8ca06f9a9916160512c8f6c8ba284458
Justin Song <justin57391@gmail.com>
2024-05-25 05:29:30 -0700

    building search page

--- snapseek/lib/main.dart
+++ snapseek/lib/main.dart
@@ -1,11 +1,13 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const SnapSeek());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class SnapSeek extends StatelessWidget {
+  const SnapSeek({super.key});
 
   // This widget is the root of your application.
   @override
@@ -13,19 +15,48 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'SnapSeek',
       theme: ThemeData(
+        textTheme: GoogleFonts.interTextTheme(Theme.of(context).textTheme),
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: const SearchPage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class SearchPage extends StatelessWidget {
+  const SearchPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar());
+    return Scaffold(
+      appBar:
+        AppBar(
+          title: const Center(child: Text('Search', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20))),
+          bottom: PreferredSize(preferredSize: const Size.fromHeight(2.0), child: Container(color: Colors.black, height: 0.5))
+        ),
+      body: const SearchBar()
+    );
   }  
+}
+
+class SearchBar extends StatefulWidget {
+  const SearchBar({super.key});
+
+  @override
+  State<SearchBar> createState() => _SearchBarState();
+}
+
+class _SearchBarState extends State<SearchBar> {
+  final controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.all(16.0),
+        child: CupertinoSearchTextField(
+          controller: controller,
+        )
+    );
+  }
 }
