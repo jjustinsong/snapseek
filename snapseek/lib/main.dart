7c030b981cf4e14a1b0ee49edd03f103d4877a9c
Justin Song <justin57391@gmail.com>
2024-06-16 16:04:45 -0700

    feed

--- snapseek/lib/main.dart
+++ snapseek/lib/main.dart
@@ -5,12 +5,15 @@ import 'package:snapseek/pages/search.dart';
 import 'package:snapseek/pages/login.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:stream_feed_flutter_core/stream_feed_flutter_core.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
   runApp(const SnapSeek());
 }
 
